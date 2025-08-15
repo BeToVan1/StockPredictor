badabc240dfb90f361d3b4ea5bea1e276d47d187
BeToVan1 <bevanto49797@gmail.com>
2025-08-14 23:07:08 -0700

    scripts changes in package.json

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,7 +11,10 @@ async function bootstrap() {
     methods: 'GET,POST',
     credentials: true,
   });
-  
+  app.use(express.static(join(__dirname, 'public')));
+  app.getHttpAdapter().get('*', (req, res) => {
+    res.sendFile(join(__dirname, '..', 'public', 'index.html'));
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
