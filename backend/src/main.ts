8ecf7096e0bd3837b28a8c4528327dcd5bc70ff3
BeToVan1 <bevanto49797@gmail.com>
2025-08-15 01:03:39 -0700

    CORS inclusion

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,10 +3,20 @@ import { AppModule } from './app.module';
 
 
 async function bootstrap() {
+  const allowedOrigins = [
+    'https://stockpredictor-frontend.onrender.com',
+    'https://stockpredictor-0lwg.onrender.com',
+  ];
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: 'https://stockpredictor-0lwg.onrender.com', // allow React frontend
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }, // allow React frontend
     methods: 'GET,POST',
     credentials: true,
   });
